2143b37c9a4e6dc2bdd9fab0f96c2a9187a5ca03
MnDMzTR <luisgarcia@alumnos.udg.mx>
2018-04-19 22:57:55 -0500

    Localizacion del problema

--- pages/actualizar_libro.php
+++ pages/actualizar_libro.php
@@ -8,26 +8,15 @@ $titulo = trim($_POST["txttitulo"]);
 $paginas = $_POST["txtpaginas"];
 $precio = $_POST["txtprecio"];
 $cantidad = $_POST["txtcantidad"];
-$aniopublicacion = $_POST["combo_aniopublicacion"];
+$anio = $_POST["combo_aniopublicacion"];
 $materia = $_POST["combo_materia"];
 $editorial = $_POST["combo_editorial"];
 $autor = $_POST["combo_autor"];
 
-$sqlUpdate = "UPDATE libros SET titulo='$titulo',npaginas=$paginas,aniopublicacion=$aniopublicacion,
-precioactual=$precio,stock=$cantidad,id_materia=$materia,id_editorial=$editorial,id_autor=$autor WHERE id_libro=".$id;
-$conn->exec($sqlUpdate);
+$sqlUpdate = "UPDATE libros SET titulo='$titulo', npaginas=$paginas, aniopublicacion=$anio, precioactual=$precio, stock=$cantidad, id_materia=$materia, id_editorial=$editorial, id_autor=$autor WHERE id_libro=".$id;
 
-echo "<script type='text/javascript'>window.location.replace('http://localhost/luisGarcia18/pages/reporte_libros.php')</script>"
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-<?php
 echo $sqlUpdate;
+//$conn->exec($sqlUpdate);
+
+//echo "<script type='text/javascript'>window.location.replace('http://localhost/luisGarcia18/pages/reporte_libros.php')</script>"
 ?>
-</body>
-</html>
---
+++ pages/registrar_autor.php
@@ -0,0 +1,43 @@
+<?php
+require_once "conn_mysql_luis.php";
+$result = "";
+
+$autor = $_POST["txtnumero"];
+$autor = (int)$autor;
+$nombre = strtoupper(trim($_POST["txtnombre"]));
+$paterno = $_POST["txtapaterno"];
+$materno = $_POST["txtamaterno"];
+$direccion = $_POST["txtdireccion"];
+$pais = $_POST["txtpais"];
+$nickname = $_POST["txtnickname"];
+
+$sqlINSERT1 = "INSERT INTO autor(id_autor,nombre,paterno,materno,direccion,pais,nickname)
+VALUES ($autor,'$nombre','$paterno','$materno','$direccion','$pais','$nickname')";
+$conn->exec($sqlINSERT1);
+?>
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Autor Registrado</title>
+    <link rel="stylesheet" href="../css/estilosa.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+<div align="center">
+    <fieldset>
+        <legend style="font-size: 22px"><b>¡Libro registrado correctamente!</b></legend>
+        <div><h3>Id Autor: <?php echo $autor?></h3></div>
+        <div><h3>Nombre: <?php echo $nombre?></h3></div>
+        <div><h3>Apellido Paterno: <?php echo $paterno?></h3></div>
+        <div><h3>Apellido Materno: <?php echo $materno?></h3></div>
+        <div><h3>Dirección: <?php echo $direccion?></h3></div>
+        <div><h3>Pais: <?php echo $pais?></h3></div>
+        <div><h3>Nickname: <?php echo $nickname?></h3></div>
+        <div>
+            <h3><a href="grabar_autor.php">...Registrar otro autor...</a></h3>
+        </div>
+    </fieldset>
+</div>
+</body>
+</html>
